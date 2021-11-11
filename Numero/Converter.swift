262c77b65fff183572f4b7eb69a4dda031618d77
Guilherme Ferreira <guiferreira1996@gmail.com>
2021-11-11 10:21:45 -0300

    Changes on Converter to test jenkins

--- Numero/Converter.swift
+++ Numero/Converter.swift
@@ -28,7 +28,8 @@
 
 class Converter {
   func convert(_ number: Int) -> String {
-    return String(repeating: "I", count: number)
+      return "I"
+//    return String(repeating: "I", count: number)
   } 
   
 }
